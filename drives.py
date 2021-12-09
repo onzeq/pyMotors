18b282f1423ba9c7e8dab23053c20d407ef0f8d1
onzeq <onzeq.v@googlemail.com>
2021-12-09 13:14:53 +0100

    .

--- drives.py
+++ drives.py
@@ -1,15 +1,16 @@
 from adafruit_motorkit import MotorKit
-
+import board
 
 def main():
-    kit     = MotorKit()
+    kit = MotorKit(i2c=board.I2C())
     m1      = kit.motor1
     m2      = kit.motor2
     m3      = kit.motor3
     m4      = kit.motor4
 
     m1.throttle = 0.3
-
+    while 1:
+        continue
 
 
 if __name__ == '__main__':
